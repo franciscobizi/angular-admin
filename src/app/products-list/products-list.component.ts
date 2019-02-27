6677f22dcb4a18641cfc37132892a127af98f46b
Francisco Bizi <taylorsoft28@gmail.com>
2019-02-27 23:20:37 +0200

    Created product-list component and user model

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../shared/products.service';
+
+@Component({
+  selector: 'app-produts-list',
+  templateUrl:'./products-list.component.html',
+  styles: []
+})
+export class ProductsListComponent implements OnInit {
+  message:string = '';
+  products : any[];
+  productsById : any[];
+  row : boolean = true;
+  constructor(private productService : ProductsService){}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(
+        res => {this.products = res['results']},
+        err => {console.log(err)}
+    );
+  }
+  /**
+   * add new product by submiting form
+   * @param name 
+   * @param price 
+   * @param amount 
+   */
+  onAddProduct(name:string, price:string, amount:string){
+    var data = {name : name, price : price, amount : amount};
+    this.productService.createProduct(data).subscribe(
+       res => {this.message = res['message']},
+       err => {console.log(err)}
+    );
+  }
+
+  /**
+   * getting product by id
+   * @param id 
+   */
+  onProductById(id : string){
+    this.productService.getProducts(id).subscribe(
+      res => {
+        this.products = null;
+        this.productsById = res['results'];
+      },
+      err => {console.log(err)}
+   );
+  }
+
+  /**
+   * changing limit of items in the page
+   * @param event 
+   */
+  onPageNumber(event){
+    var limit = event.target.value;
+    this.productService.getProductsByLimit(limit).subscribe(
+        res => {
+          this.products = res['results'];
+          console.log(res['results']);
+        },
+        err => {console.log(err)}
+    );
+  }
+
+  //close message alert
+  onCloseAlert(){
+    this.message = "";
+  }
+
+}
